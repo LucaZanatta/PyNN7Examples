10b75aa9ad463e2d528a1fae281997cf4d51bcc6
alan-stokes <alan.barry.stokes@gmail.com>
2017-03-28 14:02:59 +0100

    cleaned up mismatch betwene extra mdoels and pynn

--- examples/extra_models_examples/stdp_triplet.py
+++ examples/extra_models_examples/stdp_triplet.py
@@ -9,6 +9,7 @@ try:
     import pyNN.spiNNaker as sim
 except Exception as e:
     import spynnaker7.pyNN as sim
+    import spynnaker_extra_pynn_models as extra_sim
 
 #-------------------------------------------------------------------
 # This example uses the sPyNNaker implementation of the triplet rule
@@ -83,9 +84,9 @@ for t in delta_t:
         # Plastic Connection between pre_pop and post_pop
         # Sjostrom visual cortex min-triplet params
         stdp_model = sim.STDPMechanism(
-            timing_dependence = sim.PfisterSpikeTripletRule(
+            timing_dependence = extra_sim.PfisterSpikeTripletRule(
                 tau_plus = 16.8, tau_minus = 33.7, tau_x = 101, tau_y = 114),
-            weight_dependence = sim.AdditiveWeightDependence(
+            weight_dependence = extra_sim.AdditiveWeightDependence(
                 w_min = 0.0, w_max = 1.0, A_plus = param_scale * 0.0,
                 A_minus = param_scale * 7.1e-3,
                 A3_plus = param_scale * 6.5e-3, A3_minus = param_scale * 0.0)
