af86450ff9ee2256fa60ffcbc21964c161e8e532
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-07-03 13:14:29 +0100

    More flake8

--- examples/synfire_if_curr_exp_sEMD.py
+++ examples/synfire_if_curr_exp_sEMD.py
@@ -30,27 +30,27 @@ delay1 = 0.1
 delay2 = 2.0
 
 cell_params_lif = {'cm': cm, 'i_offset': i_offset, 'tau_m': tau_m,
-				   'tau_refrac': tau_refrac, 'tau_syn_E': current_decay,
-				   'tau_syn_I': current_decay, 'v_reset': v_reset,
-				   'v_rest': v_rest, 'v_thresh': v_thresh
-				  }
+                   'tau_refrac': tau_refrac, 'tau_syn_E': current_decay,
+                   'tau_syn_I': current_decay, 'v_reset': v_reset,
+                   'v_rest': v_rest, 'v_thresh': v_thresh
+                  }
 
 
 # neuron populations
 sEMD = p.Population(1, p.IF_curr_exp_sEMD, cell_params_lif, label="sEMD")
 input_first = p.Population(1, p.SpikeSourceArray, {'spike_times': [[0]]},
-						   label="input_first")
+                           label="input_first")
 input_second = p.Population(1, p.SpikeSourceArray, {'spike_times': [[0]]},
-						    label="input_second")
+                            label="input_second")
 
 
 # projections
 p.Projection(input_first, sEMD,
-			 p.OneToOneConnector(weights=weight, delays=delay1),
-			 target="excitatory")
+             p.OneToOneConnector(weights=weight, delays=delay1),
+             target="excitatory")
 p.Projection(input_second, sEMD,
-			 p.OneToOneConnector(weights=weight, delays=delay2),
-			 target="inhibitory")
+             p.OneToOneConnector(weights=weight, delays=delay2),
+             target="inhibitory")
 
 
 # records
@@ -103,9 +103,9 @@ pylab.show()
 
 # export csv files
 output_values = [membrane_voltage, time_voltage, curr_exc, curr_inh,
-				 time_current, spike_id, spike_time]
+                 time_current, spike_id, spike_time]
 testvalue = [cm, i_offset, tau_m, tau_refrac, current_decay,
-			 v_reset, v_rest, v_thresh]
+             v_reset, v_rest, v_thresh]
 
 csvfile = "output.csv"
 csvfile_parameter = "output_parameter.csv"
