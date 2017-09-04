7e27bd341bdd09ed713353755dd313b1125bcd58
sara <summerton.sara@gmail.com>
2017-09-04 11:06:47 +0100

    Debugging

--- examples/synfire_if_curr_exp.py
+++ examples/synfire_if_curr_exp.py
@@ -1,10 +1,12 @@
 """
 Synfirechain-like example
 """
-try:
-    import pyNN.spiNNaker as p
-except Exception as e:
-    import spynnaker7.pyNN as p
+from pacman.model.constraints.placer_constraints import ChipAndCoreConstraint
+
+# try:
+#     import pyNN.spiNNaker as p
+# except Exception as e:
+import spynnaker7.pyNN as p
 import pylab
 
 p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
@@ -40,6 +42,9 @@ populations.append(p.Population(nNeurons, p.IF_curr_exp, cell_params_lif,
                    label='pop_1'))
 populations.append(p.Population(1, p.SpikeSourceArray, spikeArray,
                    label='inputSpikes_1'))
+
+populations[0].add_placement_constraint(1, 1)
+populations[1].add_placement_constraint(4, 6)
 
 projections.append(p.Projection(populations[0], populations[0],
                    p.FromListConnector(loopConnections)))
